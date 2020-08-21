bca6dace49fdc22e5c538b8865cd15bee4460d9f
Sounish Nath <flock.sinasini@gmail.com>
2020-08-21 12:21:56 +0530

    strapi cms backend

---
+++ backend-api/educatify-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c535dc78e2378cc3767a7ebe330fda3'),
+    },
+  },
+});
