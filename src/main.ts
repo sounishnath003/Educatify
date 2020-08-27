787e2a1583603bb540d957e96468e63c80d48db9
Sounish Nath <flock.sinasini@gmail.com>
2020-08-27 21:27:20 +0530

    commonJS fix (HMR Replace)

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,29 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {
-  BootstrapModuleFn as Bootstrap,
-  hmr,
-  WebpackModule,
-} from '@ngxs/hmr-plugin';
+import { BootstrapModuleFn as Bootstrap } from '@ngxs/hmr-plugin';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
-declare const module: WebpackModule;
+// declare const module: WebpackModule;
 
 if (environment.production) {
   enableProdMode();
 }
 
-let bootstrap: Bootstrap = () =>
-  platformBrowserDynamic().bootstrapModule(AppModule);
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
 if (environment.hmr) {
-  hmr(module, bootstrap).catch((err) => console.error(err));
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
 } else {
-  bootstrap().catch((err) => console.error(err));
+  bootstrap().catch((err) => console.log(err));
 }
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
