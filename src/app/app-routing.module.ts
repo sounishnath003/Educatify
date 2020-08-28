59f6d53a4a89d650c1cef889fc853283a036ab22
Sounish Nath <flock.sinasini@gmail.com>
2020-08-29 00:24:21 +0530

    Added primarily Landing Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users/classes/:isAuth',
+    component: HomeComponent,
+    // redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
