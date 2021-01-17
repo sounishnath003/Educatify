4d1e864d4cda731a1560adf4c2a0cc78fac724dc
sounishnath003 <flock.sinasini@gmail.com>
2021-01-17 18:50:36 +0530

    fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
 import { ILoggedUser } from '../models/loggedUser.model';
 import { SharedDataService } from '../services/sharedData.service';
 
@@ -13,12 +12,15 @@ export class HomeComponent implements OnInit {
   isAuthenticated: Boolean = false;
 
   constructor(
-    private _router: ActivatedRoute,
     private matSnackBar: MatSnackBar,
     private sharedDataService: SharedDataService
   ) {}
 
   ngOnInit() {
+    this.checkIfAnyUserSignedIn();
+  }
+
+  checkIfAnyUserSignedIn(): void {
     if (sessionStorage['username']) {
       this.isAuthenticated = true;
       this.matSnackBar.open(
@@ -32,11 +34,5 @@ export class HomeComponent implements OnInit {
       this.sharedDataService.loggedInUserDetails = loggedInUser;
       console.log(this.sharedDataService.loggedInUserDetails);
     }
-  }
-
-  _authCheck() {
-    this._router.queryParams.subscribe((params) => {
-      this.isAuthenticated = params['isAuth'] == 'true' ? true : false;
-    });
   }
 }
